9701effa788a3b5a93a7e76418bdbe5d44bf75df
electricalscammer <155905329+electricalscammer@users.noreply.github.com>
2024-01-13 20:46:51 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "25451165"
-api_hash = "4b38093ddaef711814a3582c7d6d1178"
-bot_token = "6703419072:AAE-IJOeuY7b4K9PoHhEi3MsOoCAsGHRJaI"
+api_id = "24262622"
+api_hash = "50831eb3329ed9c0557aa2bc6aa34376"
+bot_token = "6503362899:AAESTFHfUm8Vv-hgQ4zYG82p8Prg6raeQP4"
