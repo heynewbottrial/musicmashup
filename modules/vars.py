484346281541df98c17626c170550df56f524d2c
heynewbottrial <164299318+heynewbottrial@users.noreply.github.com>
2024-06-03 19:27:00 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,4 @@
-api_id = "24262622"
-api_hash = "50831eb3329ed9c0557aa2bc6aa34376"
-bot_token = "6667584054:AAEblY4pEqT9pf-U5ZDNIG0vmMUzE1s1PNk"
+api_id = "28782393"
+api_hash = "09de20109edf51cf7fa090febe636842"
+bot_token = "7492520266:AAHKNdFZe4KjB42Jr1_MN6gZbmDiqlIvcaM"
+dump_channel_id = -1002046255043  # अपना गुप्त चैनल आईडी यहाँ जोड़ें
